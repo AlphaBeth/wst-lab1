1a37162f78987a146db6d9e1059e7ea503428290
Ivan Uskov <liveinkarai@gmail.com>
2019-02-04 20:59:07 +0300

    Lombok + logback.

--- standalone-jaxws/src/main/java/ru/ifmo/wst/lab1/App.java
+++ standalone-jaxws/src/main/java/ru/ifmo/wst/lab1/App.java
@@ -1,12 +1,15 @@
 package ru.ifmo.wst.lab1;
 
+import lombok.extern.slf4j.Slf4j;
 import ru.ifmo.wst.lab1.ws.TestService;
 
 import javax.xml.ws.Endpoint;
 
+@Slf4j
 public class App {
     public static void main(String[] args) {
+        log.info("Start application");
         Endpoint publish = Endpoint.publish("http://localhost:8080/TestService", new TestService());
-        System.out.println("Started!");
+        log.info("Application was successfully started");
     }
 }
