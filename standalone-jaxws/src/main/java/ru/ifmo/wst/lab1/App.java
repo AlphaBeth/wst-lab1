d9b6e67ecdcc5ac7783995be1044d4ee21dff9a1
Ivan Uskov <liveinkarai@gmail.com>
2019-02-04 21:19:43 +0300

    Create class for reading configuration properties from classpath resource.

--- standalone-jaxws/src/main/java/ru/ifmo/wst/lab1/App.java
+++ standalone-jaxws/src/main/java/ru/ifmo/wst/lab1/App.java
@@ -2,14 +2,22 @@ package ru.ifmo.wst.lab1;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.ifmo.wst.lab1.ws.TestService;
+import ru.ifmo.wst.util.Configuration;
 
 import javax.xml.ws.Endpoint;
 
 @Slf4j
 public class App {
     public static void main(String[] args) {
+        Configuration conf = new Configuration("config.properties");
+        String scheme = conf.get("scheme", "http:");
+        String host = conf.get("host", "localhost");
+        String port = conf.get("port", "8080");
+        String serviceName = conf.get("test.service.name", "TestService");
+        String baseUrl = scheme + "//" + host + ":" + port;
+        String testServiceUrl = baseUrl + "/" + serviceName;
         log.info("Start application");
-        Endpoint publish = Endpoint.publish("http://localhost:8080/TestService", new TestService());
+        Endpoint publish = Endpoint.publish(testServiceUrl, new TestService());
         log.info("Application was successfully started");
     }
 }
