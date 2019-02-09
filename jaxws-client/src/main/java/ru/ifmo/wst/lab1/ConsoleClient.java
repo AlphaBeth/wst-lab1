aea492df203b239aa3beee6c54cd4470ccfdf427
Ivan Uskov <liveinkarai@gmail.com>
2019-02-09 13:58:06 +0300

    Use automatically generated client jar in console client app.

--- jaxws-client/src/main/java/ru/ifmo/wst/lab1/ConsoleClient.java
+++ jaxws-client/src/main/java/ru/ifmo/wst/lab1/ConsoleClient.java
@@ -1,10 +1,11 @@
 package ru.ifmo.wst.lab1;
 
 import lombok.SneakyThrows;
-import ru.ifmo.wst.lab1.ws.ExterminatusEntity;
-import ru.ifmo.wst.lab1.ws.ExterminatusService;
-import ru.ifmo.wst.lab1.ws.ExterminatusServiceService;
+import ru.ifmo.wst.lab1.ws.client.standalone.ExterminatusEntity;
+import ru.ifmo.wst.lab1.ws.client.standalone.ExterminatusService;
+import ru.ifmo.wst.lab1.ws.client.standalone.ExterminatusServiceService;
 
+import javax.xml.ws.BindingProvider;
 import java.util.List;
 
 public class ConsoleClient {
@@ -12,6 +13,8 @@ public class ConsoleClient {
     public static void main(String[] args) {
         ExterminatusServiceService exterminatusService = new ExterminatusServiceService();
         ExterminatusService service = exterminatusService.getExterminatusServicePort();
+        BindingProvider bindingProvider = (BindingProvider) service;
+        bindingProvider.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:8080/EXTERMINATE");
         List<ExterminatusEntity> filter = service.filter(null, "инк", null, null, null, null);
         System.out.println(filter);
     }
