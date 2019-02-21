4c86888cda11bc9d65d7960886baecfdb8c31a34
Ivan Uskov <liveinkarai@gmail.com>
2019-02-21 11:13:00 +0300

    Add some data to init script.

--- docker-files/init.sql
+++ docker-files/init.sql
@@ -8,4 +8,16 @@ CREATE TABLE exterminatus
   date      TIMESTAMP    NOT NULL
 );
 
-CREATE SEQUENCE exterminatus_sequence;
+CREATE SEQUENCE exterminatus_sequence START WITH 5;
+
+INSERT INTO exterminatus (id, initiator, reason, method, planet, date)
+VALUES (1, 'Хорус Луперкаль', 'Начало Ереси', 'вирусная бомбадировка', 'Истваан III', '30555-11-11 00:00:00.000000');
+INSERT INTO exterminatus (id, initiator, reason, method, planet, date)
+VALUES (2, 'Инквизитор Криптман', 'Нашествие тиранидов', 'вирусная бомбадировка', 'Тетрис',
+        '40000-11-11 00:00:00.000000');
+INSERT INTO exterminatus (id, initiator, reason, method, planet, date)
+VALUES (3, 'Инквизитор Криптман', 'Нашествие тиранидов', 'вирусная бомбадировка', 'Кроделис, сегментум Ультима',
+        '40000-12-11 00:00:00.000000');
+INSERT INTO exterminatus (id, initiator, reason, method, planet, date)
+VALUES (4, 'Инквизитор Адрастиа', 'Нашествие тиранидов', 'циклонные торпеды', 'Тифон Примарис, сегментум Ультима',
+        '30555-11-11 00:00:00.000000');
